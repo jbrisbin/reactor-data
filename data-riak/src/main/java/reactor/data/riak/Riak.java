69ef68f0014750fdfd8416e633cf2a0fa781268b
Jon Brisbin <jbrisbin@gopivotal.com>
2013-05-18 17:57:06 -0500

    Reworking how tasks are executed, using Promises instead of Composable.

--- data-riak/src/main/java/reactor/data/riak/Riak.java
+++ data-riak/src/main/java/reactor/data/riak/Riak.java
@@ -1,203 +1,100 @@
 package reactor.data.riak;
 
 import com.basho.riak.client.IRiakClient;
-import com.basho.riak.client.IRiakObject;
 import com.basho.riak.client.RiakException;
-import com.basho.riak.client.RiakRetryFailedException;
 import com.basho.riak.client.bucket.Bucket;
-import com.basho.riak.client.cap.Mutation;
-import com.basho.riak.client.cap.Retrier;
-import com.basho.riak.client.convert.Converter;
-import com.basho.riak.client.operations.DeleteObject;
-import com.basho.riak.client.operations.FetchObject;
 import com.basho.riak.client.operations.RiakOperation;
-import com.basho.riak.client.operations.StoreObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.Fn;
 import reactor.core.CachingRegistry;
-import reactor.core.Composable;
+import reactor.core.Promise;
 import reactor.core.Reactor;
-import reactor.data.riak.event.DeleteEvent;
-import reactor.data.riak.event.FetchEvent;
-import reactor.data.riak.event.MergeEvent;
-import reactor.data.riak.event.StoreEvent;
 import reactor.data.riak.selector.BucketSelector;
-import reactor.fn.Function;
-import reactor.fn.Registration;
-import reactor.fn.Registry;
-import reactor.fn.Tuple;
+import reactor.fn.*;
 import reactor.fn.dispatch.Dispatcher;
 
-import java.util.Collection;
 import java.util.Iterator;
 
+import static reactor.Fn.$;
+import static reactor.Fn.U;
+import static reactor.core.Context.nextWorkerDispatcher;
+import static reactor.core.Context.threadPoolDispatcher;
+
 /**
+ * Instances of this class manage the execution of {@link RiakOperation RiakOperations} so that the user doesn't
+ * directly call {@link com.basho.riak.client.operations.RiakOperation#execute()}. All operations are asynchronous and
+ * the Reactor "worker" {@link Dispatcher} is used to ensure that operational load for the IO is
+ *
  * @author Jon Brisbin
  */
-public class Riak extends Reactor {
+public class Riak {
 
 	private final Logger           log            = LoggerFactory.getLogger(Riak.class);
 	private final Registry<Bucket> bucketRegistry = new CachingRegistry<>(null, null);
 	private final IRiakClient riakClient;
+	private final Reactor     reactor;
 
 	public Riak(IRiakClient riakClient) {
-		this.riakClient = riakClient;
+		this(riakClient, nextWorkerDispatcher());
 	}
 
 	public Riak(IRiakClient riakClient, Dispatcher dispatcher) {
-		super(dispatcher);
 		this.riakClient = riakClient;
+		this.reactor = new Reactor(dispatcher);
+		subscribeOperationConsumers();
 	}
 
-	public Composable<Bucket> fetchBucket(String bucketName) {
-		Iterator<Registration<? extends Bucket>> buckets = bucketRegistry.select(bucketName).iterator();
-		if (!buckets.hasNext()) {
-			return Composable.from(bucketName).using(getDispatcher()).build().
-					map(name -> {
-						try {
-							Bucket bucket = riakClient.fetchBucket(name).execute();
-							if (null == bucket) {
-								throw new IllegalArgumentException("Bucket named " + name + " not found.");
-							} else {
-								BucketSelector bucketSel = new BucketSelector(bucket);
-								bucketRegistry.register(bucketSel, bucket);
-								if (log.isTraceEnabled()) {
-									log.trace("Fetched bucket {}", bucket);
-								}
-								return bucket;
-							}
-						} catch (RiakRetryFailedException e) {
-							throw new IllegalStateException(e);
-						}
-					});
-		} else {
-			return Composable.from(buckets.next().getObject()).using(getDispatcher()).build();
-		}
+	public <T, O extends RiakOperation<T>> Promise<T> send(O op) {
+		Promise<T> p = new Promise<>(reactor);
+		reactor.notify("op", Fn.event(Tuple.of(op, p)));
+		return p;
 	}
 
-	public <T, O extends RiakOperation<T>> Composable<T> send(Composable<O> operation) {
-		return operation.map(op -> {
-			try {
-				T obj = op.execute();
-				if (log.isTraceEnabled()) {
-					log.trace("{} resulted in {}", op, obj);
+	public Promise<Bucket> fetchBucket(String name) {
+		Promise<Bucket> p = new Promise<>(reactor);
+		reactor.notify("/" + name + "/fetch", Fn.event(p));
+		return p;
+	}
+
+	private void subscribeOperationConsumers() {
+		reactor.on(U("/{bucket}/fetch"), (Event<Promise<Bucket>> ev) -> {
+			String bucketName = ev.getHeaders().get("bucket");
+			Promise<Bucket> promise = ev.getData();
+
+			Iterator<Registration<? extends Bucket>> buckets = bucketRegistry.select(bucketName).iterator();
+			if (!buckets.hasNext()) {
+
+				Bucket b = null;
+				try {
+					b = riakClient.fetchBucket(bucketName).execute();
+					bucketRegistry.register(new BucketSelector(b), b);
+					if (log.isTraceEnabled()) {
+						log.trace("{} fetched", b);
+					}
+					promise.set(b);
+				} catch (Throwable t) {
+					promise.set(t);
 				}
-				return obj;
-			} catch (RiakException e) {
-				throw new IllegalArgumentException(e);
+
+			} else {
+				promise.set(buckets.next().getObject());
 			}
 		});
 
-	}
-
-	public <T, O extends RiakOperation<T>> Composable<T> send(O operation) {
-		return send(Composable.from(operation).using(getDispatcher()).build());
-	}
-
-	public <T> Composable<T> store(String bucketName, T value) {
-		return store(bucketName, null, value, null, null, null);
-	}
-
-	public <T> Composable<T> store(String bucketName, String key,
-																 T value) {
-		return store(bucketName, key, value, null, null, null);
-	}
-
-	public <T> Composable<T> store(String bucketName, String key, T value, Function<Collection<T>, T> conflictResolver) {
-		return store(bucketName, key, value, conflictResolver, null, null);
-	}
-
-	public <T> Composable<T> store(String bucketName,
-																 String key,
-																 T value,
-																 Function<Collection<T>, T> conflictResolver,
-																 Converter<T> converter,
-																 Mutation<T> mutation) {
-		String uri = "/" + bucketName + "/" + key;
-		return send(
-				fetchBucket(bucketName).
-						map(bucket -> {
-							StoreObject<T> op = (null == key ? bucket.store(value) : bucket.store(key, value));
-							if (null != conflictResolver) {
-								op = op.withResolver(siblings -> {
-									T result = conflictResolver.apply(siblings);
-									Riak.this.notify(uri, new MergeEvent<T>(Tuple.of(siblings, result)));
-									return result;
-								});
-							}
-							if (null != converter) {
-								op = op.withConverter(converter);
-							}
-							if (null != mutation) {
-								op = op.withMutator(mutation);
-							}
-							return op;
-						}).
-						consume(op -> Riak.this.notify(uri, new StoreEvent<>(op)))
-		);
-	}
-
-	public <T> Composable<T> fetch(String bucketName, String key, Class<T> asType) {
-		return fetch(bucketName, key, asType, null, null);
-	}
-
-	@SuppressWarnings("unchecked")
-	public <T> Composable<T> fetch(String bucketName,
-																 String key,
-																 Class<T> asType,
-																 Function<Collection<T>, T> conflictResolver,
-																 Converter<T> converter) {
-		String uri = "/" + bucketName + "/" + key;
-		return send(
-				fetchBucket(bucketName).
-						map(bucket -> {
-							FetchObject<T> op = null == asType ? (FetchObject<T>) bucket.fetch(key) : bucket.fetch(key, asType);
-							if (null != conflictResolver) {
-								op = op.withResolver(siblings -> {
-									T result = conflictResolver.apply(siblings);
-									Riak.this.notify(uri, new MergeEvent<T>(Tuple.of(siblings, result)));
-									return result;
-								});
-							}
-							if (null != converter) {
-								op = op.withConverter(converter);
-							}
-							return op;
-						}).
-						consume(op -> Riak.this.notify(uri, new FetchEvent<>(op)))
-		);
-	}
-
-	@SuppressWarnings("unchecked")
-	public Composable<IRiakObject> fetch(String bucketName, String key) {
-		return fetch(bucketName, key, null, null, null);
-	}
-
-	@SuppressWarnings("unchecked")
-	public Composable<IRiakObject> fetch(String bucketName,
-																			 String key,
-																			 Function<Collection<IRiakObject>, IRiakObject> conflictResolver,
-																			 Converter<IRiakObject> converter) {
-		return fetch(bucketName, key, null, conflictResolver, converter);
-	}
-
-	public Composable<Void> delete(String bucketName, String key) {
-		return delete(bucketName, key, null);
-	}
-
-	public Composable<Void> delete(String bucketName, String key, Retrier retrier) {
-		String uri = "/" + bucketName + "/" + key;
-		return send(
-				fetchBucket(bucketName).
-						map(bucket -> {
-							DeleteObject op = bucket.delete(key);
-							if (null != retrier) {
-								op = op.withRetrier(retrier);
-							}
-							return op;
-						}).
-						consume(op -> Riak.this.notify(uri, new DeleteEvent(op)))
-		);
+		reactor.on($("op"), (Event<Tuple2<RiakOperation<Object>, Promise<Object>>> ev) -> {
+			try {
+				RiakOperation<Object> op = ev.getData().getT1();
+				Promise<Object> p = ev.getData().getT2();
+				Object result = op.execute();
+				if (log.isTraceEnabled()) {
+					log.trace("{} result: {}", op, result);
+				}
+				p.set(result);
+			} catch (RiakException e) {
+				throw new IllegalStateException(e);
+			}
+		});
 	}
 
 }
--- data-riak/src/test/java/reactor/data/riak/RiakTests.java
+++ data-riak/src/test/java/reactor/data/riak/RiakTests.java
@@ -1,31 +1,31 @@
 package reactor.data.riak;
 
+import com.basho.riak.client.IRiakObject;
 import com.basho.riak.client.RiakException;
 import com.basho.riak.client.RiakFactory;
 import com.basho.riak.client.bucket.Bucket;
-import com.basho.riak.client.operations.StoreObject;
 import org.junit.Before;
 import org.junit.Test;
 import reactor.core.Composable;
-import reactor.core.Context;
-import reactor.data.riak.event.StoreEvent;
-import reactor.fn.Event;
+import reactor.core.Promise;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static reactor.Fn.U;
 
 /**
  * @author Jon Brisbin
  */
 public class RiakTests {
 
-	static final long objCount = 500;
+	static final long objCount = 2000;
+
+	long start;
+	long end;
+	long elapsed;
+	long throughput;
 
 	Riak riak;
 
@@ -37,91 +37,45 @@ public class RiakTests {
 		return result;
 	}
 
+	private void startTimer() {
+		System.out.println("starting timer...");
+		start = System.currentTimeMillis();
+	}
+
+	private void endTimer(String prefix) {
+		end = System.currentTimeMillis();
+		elapsed = end - start;
+		throughput = Math.round((objCount / (elapsed * 1.0 / 1000)));
+		System.out.println(prefix + " " + throughput + "/s in " + elapsed + "ms");
+	}
+
 	@Before
 	public void setup() throws RiakException {
-		riak = new Riak(RiakFactory.pbcClient(), Context.nextWorkerDispatcher());
+		riak = new Riak(RiakFactory.pbcClient());
 	}
 
 	@Test
-	public void riakCanStoreData() throws InterruptedException {
-		Composable<?> store = riak.store("test", "test", "Hello World!");
-		await(store, 5); // wait for store to complete
+	public void canFetchBucket() throws InterruptedException {
+		CountDownLatch latch = new CountDownLatch(1);
 
-		Composable<String> fetch = riak.fetch("test", "test", String.class);
-		String hello = await(fetch, 5); // wait for fetch to complete
-
-		assertThat("result was fetched asynchronously", hello, is("Hello World!"));
-	}
-
-	@Test
-	public void riakCanDeleteData() throws InterruptedException {
-		Composable<?> store = riak.store("test", "test", "Hello World!");
-		await(store, 5); // wait for store to complete
-
-		Composable<Void> delete = riak.delete("test", "test");
-		await(delete, 5); // wait for delete to complete
-
-		Composable<String> fetch = riak.fetch("test", "test", String.class);
-		String hello = await(fetch, 5); // wait for fetch to complete
-
-		assertThat("result was fetched asynchronously", hello, is(nullValue()));
-	}
-
-	@Test
-	@SuppressWarnings({"unchecked", "rawtypes"})
-	public void riakCanNotifyConsumersOfOperations() throws InterruptedException {
-		CountDownLatch latch = new CountDownLatch(2);
-
-		riak.on(U("/test/{key}"), (Event ev) -> {
-			// only count down on stores
-			if (StoreEvent.class.isInstance(ev)) {
-				latch.countDown();
-			}
-		});
-
-		Composable<?> store = riak.store("test", "test", "Hello World!");
-		await(store, 5); // wait for store to complete
-
-		Composable<String> fetch = riak.fetch("test", "test", String.class);
-		String hello = await(fetch, 5); // wait for fetch to complete
-
-		assertThat("result was fetched asynchronously", hello, is("Hello World!"));
-
-		store = riak.store("test", "test", "Hello World!");
-		await(store, 5); // wait for store to complete
+		riak.fetchBucket("test").
+				onSuccess(bucket -> latch.countDown());
 
 		latch.await(5, TimeUnit.SECONDS);
+
 		assertThat("latch was counted down", latch.getCount(), is(0L));
 	}
 
 	@Test
-	public void riakCanSupportLargeVolumesOfWrites() throws InterruptedException {
-		List<StoreObject<?>> ops = new ArrayList<>((int) objCount);
-		List<Composable<?>> opWaits = new ArrayList<>((int) objCount);
+	public void canSendOperationsToRiak() throws InterruptedException {
+		CountDownLatch latch = new CountDownLatch(1);
 
-		Composable<Bucket> bucket = riak.fetchBucket("test");
-		await(bucket, 1);
+		Bucket b = riak.fetchBucket("test").await();
+		assertThat("bucket was retrieved", b, is(notNullValue()));
 
-		for (int i = 0; i < objCount; i++) {
-			ops.add(bucket.get().store("test" + i, "Hello World!".getBytes()));
-		}
+		IRiakObject iro  = riak.send(b.fetch("test")).await();
+		assertThat("object was retrieved", iro, is(notNullValue()));
 
-		long start = System.currentTimeMillis();
-		for (StoreObject<?> op : ops) {
-			opWaits.add(riak.send(op));
-		}
-		long end = System.currentTimeMillis();
-		long elapsed = (end - start);
-
-		System.out.println("throughput to queue: " + Math.round((objCount / (elapsed * 1.0 / 1000))) + "/s in " + elapsed + "ms");
-
-		for (Composable<?> wait : opWaits) {
-			await(wait, 1);
-		}
-		end = System.currentTimeMillis();
-		elapsed = (end - start);
-
-		System.out.println("throughput to wait: " + Math.round((objCount / (elapsed * 1.0 / 1000))) + "/s in " + elapsed + "ms");
 	}
 
 }
